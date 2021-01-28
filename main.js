d491b01ff47f23a9406224e9fbecdec32e57b800
wyjeon <wyjeon.info@gmail.com>
2021-01-28 15:23:22 +0900

    9.1. 미들웨어의 사용 - body parser

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
 var qs = require("querystring");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //routing
 app.get("/", function (request, response) {
@@ -74,17 +77,11 @@ app.get("/create", function (request, response) {
 });
 
 app.post("/create_process", function (request, response) {
-  var body = "";
-  request.on("data", function (data) {
-    body = body + data;
-  });
-  request.on("end", function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      response.redirect(`/?id=${title}`);
-    });
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+    response.redirect(`/?id=${title}`);
   });
 });
 
@@ -117,35 +114,23 @@ app.get("/update/:pageId", function (request, response) {
 });
 
 app.post("/update_process", function (request, response) {
-  var body = "";
-  request.on("data", function (data) {
-    body = body + data;
-  });
-  request.on("end", function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-      fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-        response.redirect(`/?id=${title}`);
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.redirect(`/?id=${title}`);
     });
   });
 });
 
 app.post("/delete_process", function (request, response) {
-  var body = "";
-  request.on("data", function (data) {
-    body = body + data;
-  });
-  request.on("end", function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (error) {
-      response.redirect(`/`);
-    });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect(`/`);
   });
 });
 
