0618d204298bd8353629d3988fbf86112c978276
wyjeon <wyjeon.info@gmail.com>
2021-01-28 14:29:07 +0900

    5. 상세페이지 구현

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var app = express();
 var port = 3000;
 var fs = require("fs");
 var template = require("./lib/template.js");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 //routing
 app.get("/", function (request, response) {
@@ -20,8 +22,30 @@ app.get("/", function (request, response) {
   });
 });
 
-app.get("/page", function (req, res) {
-  return res.send("/page");
+app.get("/page/:pageId", function (request, response) {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+      );
+      response.send(html);
+    });
+  });
 });
 
 app.listen(port, function () {
