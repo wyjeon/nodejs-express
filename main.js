36e6b939fd749327814356f3a7ffce9ca461fca6
wyjeon <wyjeon.info@gmail.com>
2021-01-28 14:47:41 +0900

    6. 페이지 생성 구현

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
+var qs = require("querystring");
 
 //routing
 app.get("/", function (request, response) {
@@ -44,6 +45,46 @@ app.get("/page/:pageId", function (request, response) {
             </form>`
       );
       response.send(html);
+    });
+  });
+});
+
+app.get("/create", function (request, response) {
+  fs.readdir("./data", function (error, filelist) {
+    var title = "WEB - create";
+    var list = template.list(filelist);
+    var html = template.HTML(
+      title,
+      list,
+      `
+        <form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `,
+      ""
+    );
+    response.send(html);
+  });
+});
+
+app.post("/create_process", function (request, response) {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.writeHead(302, { Location: `/?id=${title}` });
+      response.end();
     });
   });
 });
