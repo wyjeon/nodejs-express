5cd4ff9252e2b67f240476cf71e55a98605c7962
wyjeon <wyjeon.info@gmail.com>
2021-01-28 16:18:15 +0900

    12. 정적인 파일의 서비스

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var qs = require("querystring");
 var bodyParser = require("body-parser");
 var compression = require("compression");
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.get("*", function (request, response, next) {
@@ -26,7 +27,10 @@ app.get("/", function (request, response) {
   var html = template.HTML(
     title,
     list,
-    `<h2>${title}</h2>${description}`,
+    `
+      <h2>${title}</h2>${description}
+      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+    `,
     `<a href="/create">create</a>`
   );
   response.send(html);
